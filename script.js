8a0f50ee6c2fce94a083834a20d22c7989920a84
Aquaday <martin991940@hotmail.com>
2025-02-12 10:13:04 +0100

    blackjack start

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ function save() {
     count = 0
 }
 
+
+
+let card1 = 10
+let card2 = 10
+let sum = card1 + card2
+let isAlive = true
+let hasBlackJack = false
+let message = ""
+
+let messageEl = document.getElementById("message-el") 
+let sumEl = document.getElementById("sum-el") 
+let cardEl = document.getElementById("card-el") 
+
+
+let messageUnder = "Would you like to draw another card?"
+let messageWon = "Congratz, you have BlackJack!"
+let messageOver = "You lost!!!"
+
+
+function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardEl.textContent = "Cards: " + card1 + " " + card2
+    if (sum <= 20) {
+    message = messageUnder
+} else if (sum === 21) {
+    message = messageWon
+    hasBlackJack = true
+} else {
+    message = messageOver
+    isAlive = false
+}
+
+ messageEl.textContent = message
+
+
+}

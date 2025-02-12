bd64ac61c7c8a71205d65ed2a116f67d965f0953
Aquaday <martin991940@hotmail.com>
2025-02-12 11:43:14 +0100

    .

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function save() {
 
 let card1 = 10
 let card2 = 10
+let cards = [card1, card2]
 let sum = card1 + card2
 let isAlive = true
 let hasBlackJack = false
@@ -38,8 +39,16 @@ let messageOver = "You lost!!!"
 
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardEl.textContent = "Cards: " + card1 + " " + card2
+    cardEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        console.log(cards[i])
+        cardEl.textContent += cards[i] + " "
+    }
     if (sum <= 20) {
     message = messageUnder
 } else if (sum === 21) {
@@ -49,8 +58,32 @@ function startGame() {
     message = messageOver
     isAlive = false
 }
-
  messageEl.textContent = message
-
-
 }
+
+function drawCard() {
+    console.log("drawing Card")
+    let newCard = 2
+    sum += newCard
+    cards.push(newCard)
+    startGame()
+}
+
+
+
+// let cardsss = [7, 3, 9]
+
+// // Create a for loop that logs out all the cards in the array
+// // Use cards.length to specify how long the loop should run
+
+// for (let i = 0; i < cardsss.length; i += 1) {
+//     console.log(cardsss[i])
+// }
+
+// let tall = [1, 5, 4, 8, 2, 4, 6]
+
+// for (let i = 0; i < tall.length; i += 1) {
+//     console.log(tall[i])
+// }
+
+
